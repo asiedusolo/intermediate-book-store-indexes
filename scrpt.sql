6d1b354b46fb21bed2c3a3709cbe2c2f7bbbd75a
asiedusolo <solomon.asiedu@amalitech.org>
2021-10-25 20:10:42 -0400

    adding partial index

--- scrpt.sql
+++ scrpt.sql
@@ -18,3 +18,25 @@ WHERE tablename = 'orders';
 SELECT * 
 FROM pg_Indexes
 WHERE tablename = 'books';
+
+XPLAIN ANALYZE 
+SELECT customer_id, quantity
+FROM orders
+WHERE quantity > 18
+ORDER BY customer_id, quantity;
+
+CREATE INDEX orders_quantity_greater_than_18 
+ON orders(quantity)
+WHERE quantity > 18;
+
+EXPLAIN ANALYZE 
+SELECT customer_id, quantity
+FROM orders
+WHERE quantity > 18
+ORDER BY customer_id, quantity;
+
+
+EXPLAIN ANALYZE
+SELECT *
+FROM customers
+WHERE customer_id < 100;
